56de0d9e14f727f5027496df3d0bdcf13e097427
DeokHwan Kim <tiger1710p@gmail.com>
2022-10-10 15:24:23 +0900

    수 정렬하기

---
+++ dhkim/10989.rs
@@ -0,0 +1,23 @@
+use std::io;
+use std::io::Write;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap(); // scanf
+    let n = input.trim().parse::<usize>().unwrap();
+
+    let mut nums: [usize; 10000] = [0; 10000];
+    for _ in 0..n {
+        input.clear();
+        io::stdin().read_line(&mut input).unwrap(); // scanf
+        nums[input.trim().parse::<usize>().unwrap() - 1] += 1;
+    }
+
+    let mut output = io::BufWriter::with_capacity(4096000, io::stdout());
+    for i in 0..10000 {
+        if 0 < nums[i] {
+            for _ in 0..nums[i] {
+                writeln!(output, "{}", i + 1).unwrap();
+            }
+        }
+    }
