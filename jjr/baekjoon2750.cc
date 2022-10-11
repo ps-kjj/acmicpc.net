b8ff59080f4367ea58757ac41addf9e92971be7b
dev_whale <jeong22437747@gmail.com>
2022-10-11 10:47:43 +0900

    1920.cpp -> 2750.cc change name

---
+++ jjr/baekjoon2750.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int arr[1000];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	
+	for (int i = n-1; i > 0; i--)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[j] > arr[j+1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < n ; i++)
+	{
+		cout << arr[i] << endl;
+	}
+}
+
