e9dbaef7aa7ab17a4f51f99984b803cd1413916f
Meda <medAndro@protonmail.com>
2022-10-14 13:42:31 +0900

    Update 10845.py
    중요한건 눈에 보이지 않아

--- medAndro/10845.py
+++ medAndro/10845.py
@@ -3,18 +3,18 @@ input = sys.stdin.readline
 print = sys.stdout.write
 
 n = int(input().rstrip())
-stack = []
+queue = []
 len = 0
 for i in range(n):
     cmd= input().rstrip()
     if "push" in cmd:
-        stack.append(cmd.split()[1])
+        queue.append(cmd.split()[1])
         len +=1
     elif "pop" == cmd:
         if len == 0:
             print("%s\n" % -1)
         else:
-            print("%s\n" % stack.pop(0))
+            print("%s\n" % queue.pop(0))
             len -= 1
     elif "size" == cmd:
         print("%s\n" % len)
@@ -27,14 +27,14 @@ for i in range(n):
         if len == 0:
             print("%s\n" % -1)
         else:
-            print("%s\n" % stack[0])
+            print("%s\n" % queue[0])
     elif "back" == cmd:
         if len == 0:
             print("%s\n" % -1)
         else:
-            print("%s\n" % stack[len-1])
+            print("%s\n" % queue[len-1])
     else:
         if len == 0:
             print("%s\n" % -1)
         else:
-            print("%s\n" % stack[len-1])
+            print("%s\n" % queue[len-1])
