76b77717d93d4404581db5018b4135871f7f323b
dev_whale <jeong22437747@gmail.com>
2022-10-31 19:07:43 +0900

    9012 파일 수정

--- jjr/boj9012.java
+++ jjr/boj9012.java
@@ -1,25 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.StringTokenizer;
-import java.util.Arrays;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        HashSet<Integer> set = new HashSet<>();
-
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            set.add(Integer.parseInt(st.nextToken()));
-        }
+        StringBuilder sb = new StringBuilder();
 
-        Integer[] arr = set.toArray(new Integer[set.size()]);
-        Arrays.sort(arr);
-        for (Integer i : arr) {
-            System.out.print(i + " ");
+        for (int i = 0; i < n; i++) {
+            Stack<Character> stack = new Stack<>();
+            boolean isVPS = true;
+            String str = br.readLine();
+            for (int j = 0; j < str.length(); j++) {
+                if(str.charAt(j) == '('){
+                    stack.push(str.charAt(j));
+                }else{
+                    if(!stack.isEmpty()){
+                        stack.pop();
+                    }else{
+                        isVPS = false;
+                        break;
+                    }
+                }
+            }
+            if(!stack.isEmpty()){
+                isVPS = false;
+            }
+            if(isVPS){
+                sb.append("YES").append(" ");
+            }else{
+                sb.append("NO").append(" ");
+            }
+
         }
+        System.out.println(sb);
     }
 }
+
