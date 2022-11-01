15bac4064e96bbad9af64cd277d8477464f32dca
dev_whale <jeong22437747@gmail.com>
2022-11-01 17:46:17 +0900

    10816 map.getOrDefault 활용
    시간 1252ms -> 1136ms 약간의 성능 향상

--- jjr/boj10816.java
+++ jjr/boj10816.java
@@ -2,10 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
-public class Main {
+public class boj10816 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -16,22 +15,14 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             int nt = Integer.parseInt(st.nextToken());
-            if(map.containsKey(nt)){
-                map.put(nt, map.get(nt) + 1);
-            }else{
-                map.put(nt, 1);
-            }
+            map.put(nt, map.getOrDefault(nt,0)+1);
         }
         int m  = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
             int nt = Integer.parseInt(st.nextToken());
-            if(map.containsKey(nt)){
-                sb.append(map.get(nt)).append(" ");
-            }else{
-                sb.append(0).append(" ");
-            }
+            sb.append(map.getOrDefault(nt, 0)).append(" ");
         }
         System.out.println(sb);
     }
